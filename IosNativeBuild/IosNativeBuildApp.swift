a196baf4332bc4b8d7e32e50e9dc4e0d116870af
Amit Kundu <amit.kundu@f-secure.com>
2026-01-18 11:33:16 +0200

    Initial Commit

---
+++ IosNativeBuild/IosNativeBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosNativeBuildApp.swift
+//  IosNativeBuild
+//
+//  Created by Amit Kundu on 18.1.2026.
+//
+
+import SwiftUI
+
+@main
+struct IosNativeBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
